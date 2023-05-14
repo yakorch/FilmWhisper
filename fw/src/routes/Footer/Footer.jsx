8f7d5633640156347ff6e3a3664ddcb0ec1b7f1d
MatthewPr12 <matvii.prytula@ucu.edu.ua>
2023-05-15 01:47:21 +0300

    footer added

--- fw/src/routes/Footer/Footer.jsx
+++ fw/src/routes/Footer/Footer.jsx
@@ -1,9 +1,42 @@
+import { AppBar, Toolbar, Typography, IconButton, Box, Container } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
+const styles = {
+    footer: {
+        top: "auto",
+        bottom: 0,
+        marginTop: "auto",
+        backgroundColor: "primary",
+    },
+    grow: {
+        flexGrow: 1,
+    },
+};
+
 export function Footer() {
     return (
-        <>
-            <h3>
-                I am giant foot (errrr....)
-            </h3>
-        </>
-    )
+        <Box sx={{ flexGrow: 1 }}>
+            <Container>
+                {/* Your page content goes here */}
+            </Container>
+            <AppBar position="static" color="primary" style={styles.footer}>
+                <Toolbar>
+                    <IconButton
+                        href="https://github.com/your-github-repo"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        edge="start"
+                        color="inherit"
+                        aria-label="GitHub"
+                    >
+                        <GitHubIcon />
+                    </IconButton>
+                    <div style={styles.grow} />
+                    <Typography variant="subtitle2" color="inherit">
+                        © 2023 Film Whisper. All rights reserved.
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 }
