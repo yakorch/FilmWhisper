838a9bf4616f8c3e74146a85fb27c19a449fb144
qLate <uaolegikua@gmail.com>
2023-05-15 09:02:16 +0300

    added faq

--- fw/src/routes/FAQSection/FAQSection.jsx
+++ fw/src/routes/FAQSection/FAQSection.jsx
@@ -1,9 +1,25 @@
+import React from "react";
+import { MDBAccordion, MDBAccordionItem, MDBContainer } from "mdb-react-ui-kit";
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+
 export function FAQSection() {
     return (
-        <>
-            <h2>
-                Питання?
-            </h2>
-        </>
+        <MDBAccordion alwaysOpen initialActive={1} style={{ margin: "50px 0 50px 0" }}>
+            <MDBAccordionItem collapseId={1} headerTitle="Do you like Film Whisper?">
+                <strong>Hell yeah.</strong>
+            </MDBAccordionItem>
+            <MDBAccordionItem collapseId={2} headerTitle="Do you like Film Whisper?">
+                <strong>Hell yeah.</strong>
+            </MDBAccordionItem>
+            <MDBAccordionItem collapseId={3} headerTitle="Do you like Film Whisper?">
+                <strong>Hell yeah.</strong>
+            </MDBAccordionItem>
+            <MDBAccordionItem collapseId={4} headerTitle="Do you like Film Whisper?">
+                <strong>Hell yeah.</strong>
+            </MDBAccordionItem>
+            <MDBAccordionItem collapseId={5} headerTitle="Do you like Film Whisper?">
+                <strong>Hell yeah.</strong>
+            </MDBAccordionItem>
+        </MDBAccordion>
     )
 }
