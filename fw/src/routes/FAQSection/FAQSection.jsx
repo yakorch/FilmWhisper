feb1e652ab127369bbc9f7f2024a44bab2f8be02
yakorch <korchyar19@gmail.com>
2023-05-15 12:56:25 +0300

    real questions in FAQ

--- fw/src/routes/FAQSection/FAQSection.jsx
+++ fw/src/routes/FAQSection/FAQSection.jsx
@@ -1,25 +1,27 @@
 import React from "react";
-import { MDBAccordion, MDBAccordionItem, MDBContainer } from "mdb-react-ui-kit";
+import {MDBAccordion, MDBAccordionItem} from "mdb-react-ui-kit";
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
 export function FAQSection() {
+    const questionsAndAnswers = [
+        {question: "Do you like Film Whisper?", answer: "Hell yeah."},
+        {question: "What mathematical operation is performed on genres?", answer: "By default - Intersection. We are working on creating a Union too."},
+        {question: "What is the limit of the recommended movies?", answer: "10! (hehe)"},
+        {question: "Why do I need a User account?", answer: "To save your favourite movies!"},
+        {question: "Can I add imaginary actors to the Actors filter?", answer: "Yes, but you won't get any results ;)"},
+    ];
+
     return (
-        <MDBAccordion alwaysOpen initialActive={1} style={{ margin: "50px 0 50px 0" }}>
-            <MDBAccordionItem collapseId={1} headerTitle="Do you like Film Whisper?">
-                <strong>Hell yeah.</strong>
-            </MDBAccordionItem>
-            <MDBAccordionItem collapseId={2} headerTitle="Do you like Film Whisper?">
-                <strong>Hell yeah.</strong>
-            </MDBAccordionItem>
-            <MDBAccordionItem collapseId={3} headerTitle="Do you like Film Whisper?">
-                <strong>Hell yeah.</strong>
-            </MDBAccordionItem>
-            <MDBAccordionItem collapseId={4} headerTitle="Do you like Film Whisper?">
-                <strong>Hell yeah.</strong>
-            </MDBAccordionItem>
-            <MDBAccordionItem collapseId={5} headerTitle="Do you like Film Whisper?">
-                <strong>Hell yeah.</strong>
-            </MDBAccordionItem>
+
+
+        <MDBAccordion alwaysOpen initialActive={1} style={{margin: "50px 0 50px 0"}}>
+            {
+                questionsAndAnswers.map(({question, answer}, index) =>
+
+                    <MDBAccordionItem collapseId={index + 1} headerTitle={question}>
+                        <strong>{answer}</strong>
+                    </MDBAccordionItem>)
+            }
         </MDBAccordion>
     )
 }
