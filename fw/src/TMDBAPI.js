5abfd81a455d817f43b6170012f2281cda7e412f
yakorch <korchyar19@gmail.com>
2023-05-20 17:29:56 +0300

    API key removed in func arguments

--- fw/src/TMDBAPI.js
+++ fw/src/TMDBAPI.js
@@ -22,30 +22,26 @@ const genresMap = {
     "Western": 37
 };
 
-function getTopRatedMovies(apiKey, numMovies) {
-    return fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`)
+function getTopRatedMovies(numMovies) {
+    return fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`)
         .then(response => response.json())
         .then(data => data.results.slice(0, numMovies));
 }
 
-function getTopRatedMoviesByGenres(apiKey, numMovies, genres) {
+function getTopRatedMoviesByGenresIntersection(numMovies, genres) {
     // First, get the genre ids by name
     const genreIds = genres.map(genreName => genresMap[genreName]).filter(id => id !== undefined);
 
     // Then, get the top-rated movies for those genres
-    return fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=vote_average.desc&vote_count.gte=100&with_genres=${genreIds.join(',')}`)
+    return fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=vote_average.desc&vote_count.gte=100&with_genres=${genreIds.join(',')}`)
         .then(response => response.json())
         .then(data => data.results.slice(0, numMovies));
 }
 
-function getTopRatedMoviesByGenresHardcoded(genres) {
-    return getTopRatedMoviesByGenresUnion(API_KEY, 10, genres)
-}
-
-function getTopRatedMoviesByGenresUnion(apiKey, numMovies, genres) {
+function getTopRatedMoviesByGenresUnion(numMovies, genres) {
     const promises = genres.map(genreName => {
         const genreId = genresMap[genreName];
-        return fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=vote_average.desc&vote_count.gte=100&with_genres=${genreId}`)
+        return fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=vote_average.desc&vote_count.gte=100&with_genres=${genreId}`)
             .then(response => response.json())
             .then(data => data.results);
     });
@@ -59,9 +55,9 @@ function getTopRatedMoviesByGenresUnion(apiKey, numMovies, genres) {
         });
 }
 
-function getActorIdByName(apiKey, actorName) {
+function getActorIdByName(actorName) {
     return fetch(
-        `https://api.themoviedb.org/3/search/person?api_key=${apiKey}&query=${encodeURIComponent(
+        `https://api.themoviedb.org/3/search/person?api_key=${API_KEY}&query=${encodeURIComponent(
             actorName
         )}`
     )
@@ -75,18 +71,18 @@ function getActorIdByName(apiKey, actorName) {
         });
 }
 
-function getMoviesByActorId(apiKey, actorId, numMovies) {
+function getMoviesByActorId(actorId, numMovies) {
     return fetch(
-        `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&with_cast=${actorId}`
+        `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&with_cast=${actorId}`
     )
         .then((response) => response.json())
         .then((data) => data.results.slice(0, numMovies));
 }
 
-function getMoviesByActorNames(apiKey, actorNames, numMovies) {
+function getMoviesByActorNames(actorNames, numMovies) {
     const promises = actorNames.map((actorName) =>
-        getActorIdByName(apiKey, actorName).then((actorId) =>
-            getMoviesByActorId(apiKey, actorId, numMovies)
+        getActorIdByName(actorName).then((actorId) =>
+            getMoviesByActorId(actorId, numMovies)
         )
     );
 
@@ -96,10 +92,8 @@ function getMoviesByActorNames(apiKey, actorNames, numMovies) {
 
 export {
     getTopRatedMovies,
-    getTopRatedMoviesByGenres,
-    getTopRatedMoviesByGenresHardcoded,
+    getTopRatedMoviesByGenresIntersection,
     getTopRatedMoviesByGenresUnion,
     getMoviesByActorNames,
-    API_KEY,
     genresMap
 };
